1cc19068f7b8bb75b1c1cf8dedbfad24250216c3
KEI <nishimoto_al@yahoo.co.jp>
2018-12-10 00:19:29 -0800

    Implement Phenotype (w/o test)

---
+++ src/main/Phenotype.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+
+public class Phenotype {
+	int numInputs;
+	int numNeurons;
+	double [][] weights;
+	double [] rule;
+	Random rand;
+
+	Phenotype(Genotype g){
+		this.numInputs = g.numInputs;
+		this.numNeurons = g.numNeurons;
+		this.weights = transformWeights(g.weights);
+		this.rule = g.rule;
+		this.rand = new Random();
+	}
+
+	double [][] transformWeights(double [][] weights) {
+		double [][] ret = new double[weights.length][weights[0].length];
+		double ulim = 10.0;
+		double llim = 0.01;
+		for (int i = 0; i < weights.length; i ++) {
+			for (int j = 0; j < weights[0].length; j ++) {
+				ret[i][j] = transform(weights[i][j], ulim, llim);
+			}
+		}
+		return ret;
+	}
+
+	double [] transformRule(double [] rule){
+		double [] ret = new double[rule.length];
+		double ulim = 30.0;
+		double llim = 0.1;
+		for (int i = 0; i < rule.length; i ++) {
+			ret[i] = transform(rule[i], ulim, llim);
+		}
+		if (rule[0] == 0) {
+			ret[0] = (this.rand.nextDouble() + 0.5) * Math.sin(this.rand.nextGaussian());
+		}
+		if (isAll(ret, 0, 1, 5)) {
+			for (int i = 1; i < 5; i ++) {
+				ret[i] = (this.rand.nextDouble() + 0.5) * Math.sin(this.rand.nextGaussian());
+			}
+
+		}
+		return ret;
+	}
+
+	boolean isAll(double [] a, double v, int start, int end) {
+		if (start >= a.length || end > a.length) {
+			System.err.println("[ERROR] start and end should be in the range !");
+			System.exit(1);
+		}
+		for (int i = start; i < end; i ++) {
+			if (a[i] != v) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	double transform(double gx, double ulim, double llim) {
+		double x = Math.pow(gx, 3);
+		if (Math.abs(x) > ulim) {
+			return Math.sin(x) * ulim;
+		}else {
+			if (Math.abs(x) > llim) {
+				return x;
+			}else {
+				return 0;
+			}
+		}
+	}
+}
