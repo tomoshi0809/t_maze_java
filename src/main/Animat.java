7ec507899c2e8a375f247c2a190397d90fd20713
KEI <nishimoto_al@yahoo.co.jp>
2018-12-11 02:30:41 -0800

    Implement Animat skelton

--- src/main/Animat.java
+++ src/main/Animat.java
@@ -1,6 +1,31 @@
 
 public class Animat extends Phenotype {
+	double [] state;
+	boolean plastic;
+
 	Animat (Genotype g){
 		super(g);
+		this.state = new double[this.numNeurons];
+		this.plastic = true;
+	}
+
+	double behave(double [] inputs) {
+		return 0;
+	}
+
+	double [][] learn(double []inputs, double [] output){
+		return null;
+	}
+
+	int apply_rule(int x, int y) {
+		return 0;
+	}
+
+	double [] inspect_weight(double weight) {
+		return null;
+	}
+
+	double [] perform(double [] input) {
+		return null;
 	}
 }
