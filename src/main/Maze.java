6beba55225e0c95cc03c501abebf237f0a36d420
KEI <nishimoto_al@yahoo.co.jp>
2018-12-11 02:31:05 -0800

    Implement Maze skelton

--- src/main/Maze.java
+++ src/main/Maze.java
@@ -1,3 +1,6 @@
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
 abstract class Environment {
 	int numInputs;
 	abstract public int evaluate(Phenotype p);
@@ -13,5 +16,34 @@ abstract public class Maze extends Environment {
 		this.numThink = numThink;
 		this.noiseStd = noiseStd;
 		this.debug = false;
+	}
+
+	boolean home(Animat animat) {
+		return true;
+	}
+
+	boolean corridor(Animat animat) {
+		return true;
+	}
+
+	boolean junction(Animat animat) {
+		return true;
+	}
+
+	double maze_end(Animat animat) {
+		return 0;
+	}
+
+	double thinking(Animat animat, RealMatrix input) {
+		input = input.add(noise(animat));
+		return 0;
+	}
+
+	RealMatrix noise(Animat animat) {
+		double [] noise = new double[this.numInputs];
+		for (int i = 0; i < noise.length; i ++){
+			noise[i] = Math.random() * this.noiseStd;
+		}
+		return MatrixUtils.createRowRealMatrix(noise);
 	}
 }
