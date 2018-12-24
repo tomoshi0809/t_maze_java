a80cff0272778d96cb2f770eb7ae9d288a8b81b2
KEI <nishimoto_al@yahoo.co.jp>
2018-12-24 04:37:03 -0800

    BUGFIX

--- src/main/Maze.java
+++ src/main/Maze.java
@@ -32,7 +32,7 @@ abstract public class Maze extends Environment {
 	}
 
 	double thinking(Animat animat, double [][] inputs) {
-		double [][] output;
+		double [][] output = null;
 		for (int i = 0; i < this.numThink; i ++ ) {
 			double [][] a = Matrix.add(inputs, noise());
 			output = animat.perform(a);
