2c1dd0cff88550421f679a1ef9d199d606118b38
KEI <nishimoto_al@yahoo.co.jp>
2019-01-18 03:33:46 -0800

    Initial commit for adding DataTag to Geonotype

---
+++ src/main/Data.java
@@ -0,0 +1,14 @@
+
+public class Data {
+	double sucRateRight;
+	double sucRateLight;
+	
+	Data(){
+		this.sucRateRight = 0;
+		this.sucRateLight = 0;
+	}
+	
+	void putData(int target, boolean isSuccess) {
+		
+	}
+}
